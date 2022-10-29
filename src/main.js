b6a8b74adb786e74fbd5fb624b042c9961b8e242
minhee.park <minhee.park@waiker.ai>
2022-10-29 17:55:42 +0900

    - 디자인 검수 수정

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue';
 import router from './routers/router';
 // import store from 'stores/store';
 import './assets/style/main.scss'; //공통적으로 사용되는 UI들만 main.scss에 @import합니다.
-import 'wowjs/css/libs/animate.css'
+import 'wowjs/css/libs/animate.css';
+import ScrollParallax from 'vue3-parallax/src/components/ScrollParallax.vue';
 
 const app = createApp(App);
+app.component('scroll-parallax', ScrollParallax);
 app.use(router);
 // app.use(store);
 app.mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 3000
+  },
+  chainWebpack(config) {
+    config.resolve.symlinks(false)
+    config.resolve.alias.set( 'vue', path.resolve('./node_modules/vue'))
   },
   lintOnSave: false //lint 에러 발생으로 주석처리
 })
